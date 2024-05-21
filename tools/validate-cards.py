4c81692beaa3917b3f0a62035164cb5f04ac10d6
sobolevn <mail@sobolevn.me>
2024-05-21 09:06:27 +0300

    Fix CI

--- tools/validate-cards.py
+++ tools/validate-cards.py
@@ -14,7 +14,6 @@ def _read_card_counts(cards_path: str) -> list[int]:
     card_counts = []
     for card_sheet in document.sheets:
         cell_value = card_sheet.tables[0].cell("E18").value
-        print(cell_value)
         if cell_value.startswith(" ="):  # `= number of players`
             card_counts.append(_NUMBER_OF_PLAYERS)
         else:
